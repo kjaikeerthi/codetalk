7fa7fee2b4a2cd78abd46b4cff3dd3f3e5aaf71a
Jai Keerthi <me@jaikeerthi.in>
2011-07-17 16:58:18 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codetalk::Application.config.secret_token = '7d2d31de1351282e528e26515be1a0473c7844bbba0378fe564f09cbb23b041ec52117fd35eaae93341331616a97fa5dd66a679331f7a6265cc0b44de6a5f51d'
