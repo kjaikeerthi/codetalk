84ea1480bf3f1ca6fe9378210eb5e5c2fa84edd4
Jai Keerthi <me@jaikeerthi.in>
2011-07-18 10:29:49 +0530

    added git model

---
+++ app/models/git.rb
@@ -0,0 +1,23 @@
+class Git
+  include Mongoid::Document
+
+  field :uid
+  field :token
+  field :git_url
+  field :blog
+  field :type
+  field :login
+  field :email
+  field :company
+  field :name
+  field :gravatar_id
+  field :collaborators
+  field :public_repo_count
+  field :public_gist_count
+  field :following_count
+  field :owned_private_repo_count
+  field :total_private_repo_count
+  field :followers_count
+  field :private_gist_count
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   field :email
   field :username
+  field :github_login
+  field :github_token
+  belongs_to :git
   attr_accessible :email, :password, :password_confirmation, :remember_me
   validates_presence_of :email
   validates_uniqueness_of :email, :username, :case_sensitive => false
